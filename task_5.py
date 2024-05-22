3741cfd0e7d6edb1fd0435e27fc492f6bee46c93
Tsymliakov Denis <denis.tsym@mail.ru>
2024-05-22 11:54:59 +0300

    fix: solved task 5

--- task_5.py
+++ task_5.py
@@ -25,22 +25,46 @@
 # стыковкарась
 
 
-def get_words(path):
-    words = []
+MIN_SIMMILAR_CHARS = 2
 
-    with open("task_5.txt", 'r', encoding='utf-8') as f:
+def get_words(path: str) -> list[str]:
+    with open(path, 'r', encoding='utf-8') as f:
         return f.read().splitlines()
 
-def get_mutated_words(user_word, words):
-    """ В реальности, конечно, лучше применить другой подход для генерации слов,
-    например, подход, основанный на цепях Маркова.
-    """
-    mutated_words = []
+def get_mutated_words(user_word: str, words: str) -> list[str]:
+    continuing_words : list[str] = []
 
     for w in words:
-        mutated_words.append(user_word[:int(len(user_word) / 2)] + w[int(len(w) / 2):])
+        if w == user_word:
+            continue
 
-    return mutated_words
+        if MIN_SIMMILAR_CHARS == 0:
+            continuing_words.append(user_word + w)
+            continue
+
+        user_word_char_idx = 0
+
+        while user_word_char_idx < len(user_word):
+            simmillar_chars = 0
+            w_char_idx = 0
+
+            while user_word_char_idx < len(user_word) and w_char_idx < len(w):
+                if user_word[user_word_char_idx] != w[w_char_idx]:
+                    break
+
+                simmillar_chars += 1
+                user_word_char_idx += 1
+                w_char_idx += 1
+
+                if user_word_char_idx == len(user_word):
+                    if simmillar_chars >= MIN_SIMMILAR_CHARS:
+                        continuing_words.append(user_word + w[simmillar_chars:])
+                        break
+                    break
+
+            user_word_char_idx += 1
+
+    return continuing_words
 
 if __name__ == "__main__":
     words = get_words('./task_5.txt')
